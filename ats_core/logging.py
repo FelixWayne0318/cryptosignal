9dc28c01878a9e434583415ad3e44a611f1a0c33
FelixWayne0318 <109026622+FelixWayne0318@users.noreply.github.com>
2025-10-22 02:07:04 +0800

    创建 logging.py

---
+++ ats_core/logging.py
@@ -0,0 +1,11 @@
+import sys, time
+
+def log(msg, *args):
+    ts = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    s = msg if not args else msg % args
+    print(f"[{ts}Z] {s}", file=sys.stdout, flush=True)
+
+def warn(msg, *args):
+    ts = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    s = msg if not args else msg % args
+    print(f"[{ts}Z][WARN] {s}", file=sys.stderr, flush=True)
